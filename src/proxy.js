d5efa9ec596e757a4e2456b158317481757c6caf
Sam Ko <samko0927@gmail.com>
2023-04-05 10:10:31 +0800

    4/5 sam3 commit

--- src/proxy.js
+++ src/proxy.js
@@ -12,4 +12,4 @@ export const JsonSecret = process.env.REACT_APP_JSON_SECRET
 export const gh_APP_ID = process.env.REACT_APP_github_Client_ID
 export const gh_APP_SECRET = process.env.REACT_APP_github_SECRET
 //換成.env 用這隻api打回來才會是回到local連接不然會用到 回正式網的連結
-export const baseURL = 'https://chiioa-api.onrender.com/api/v1/'
+export const callbackURL = 'https://chiioa-api.onrender.com/api/v1/'
